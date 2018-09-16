d7a8f0df6d8bc325ca7aacbfe24f88d87dc34fad
Lior5 <31022527+Lior5@users.noreply.github.com>
2018-09-16 15:25:25 -0400

    Create a.java

---
+++ Codeforces/Round Sample/a.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class R505A {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int[] arr =new int[26];
+		String s = scan.next();
+		for(int i = 0;i<a;i++){
+			arr[s.charAt(i)-'a']++;
+		}
+		int count = 0;
+		int count1 = 0;
+		for(int i = 0;i<26;i++){
+			if(arr[i]==1){
+				count++;
+			}else{
+				if(arr[i]>1){
+					count1++;
+				}
+			}
+		}
+		if(count>1 && count1==0){
+			System.out.println("No");
+			
+		}else{
+			System.out.println("Yes");
+		}
+	}
+
+}
